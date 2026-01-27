3aea210ce6980a32c7ffc3a92fc4435f27ebd2e3
st921 <syou.tana0921@icloud.com>
2026-01-28 02:26:06 +0900

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by しょう on 2026/01/28.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
